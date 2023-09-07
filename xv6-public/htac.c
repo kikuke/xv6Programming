36ac7f1e00ccba483ea44dad3b294014bf0d5975
kikuke <kkkjs0814@naver.com>
2023-09-08 01:01:55 +0900

    [Add] htac

--- xv6-public/htac.c
+++ xv6-public/htac.c
@@ -6,17 +6,27 @@
 int line;
 
 /**
+ * offset이 0이면 더이상 뒤로 밀리지 않고 0을 리턴
+ * c에 offset 앞의 값을 저장해줍니다.
  * rewind and return now offset
  * return -1 when occured error
 */
 off_xvt
 rewindchar(int fd, char *c)
 {
+    off_xvt now_off;
+
+    if((now_off = lseek(fd, 0, SEEK_CUR)) < 0) {
+        printf(2, "rewindchar: lseek error\n");
+        return -1;
+    }
+    if(now_off <= 0)
+        return now_off;
+
     if(lseek(fd, -1, SEEK_CUR) < 0) {
         printf(2, "rewindchar: lseek error\n");
         return -1;
     }
-    
     if(read(fd, c, 1) < 0) {
         printf(2, "rewindchar: read error\n");
         return -1;
@@ -24,18 +34,17 @@ rewindchar(int fd, char *c)
     return lseek(fd, -1, SEEK_CUR);
 }
 
+/**
+ * 한 라인을 되돌립니다.
+ * 오류시 -1 리턴
+ * offset이 0이라면 라인을 되돌리지 않고 0 리턴
+ * 성공시 현재 offset 리턴
+*/
 off_xvt
 rewindline(int fd)
 {
     off_xvt now_off;
     char c;
-
-    if((now_off=lseek(fd, 0, SEEK_CUR)) < 0) {
-        printf(2, "htac: lseek error\n");
-        return now_off;
-    }
-    if(now_off <= 0)
-        return now_off;
 
     if((now_off = rewindchar(fd, &c)) < 0) {
         printf(2, "rewindline: rewindchar error\n");
@@ -51,7 +60,7 @@ rewindline(int fd)
             break;
         }
     }
-    return now_off;
+    return lseek(fd, 0, SEEK_CUR);
 }
 
 void
@@ -93,7 +102,7 @@ htac(int fd)
         if(rewind_off == 0)
             return;
 
-        if ((rewind_off = rewindline(fd)) < 0) {
+        if (lseek(fd, rewind_off, SEEK_SET)) {
             printf(2, "htac: rewindline error\n");
             exit();
         }
