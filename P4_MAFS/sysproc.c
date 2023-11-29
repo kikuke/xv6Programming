7ea39bf62ae1f280fe9eea6384f469768388237f
kikuke <kkkjs0814@naver.com>
2023-11-29 10:04:58 +0900

    [MOD] 함수 위치

--- P4_MAFS/sysproc.c
+++ P4_MAFS/sysproc.c
@@ -89,3 +89,38 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+int
+sys_ssualloc(void)
+{
+  int allocsz;
+  uint oldsz, newsz;
+  struct proc *curproc = myproc();
+
+  if(argint(0, &allocsz) < 0 || allocsz <= 0 || allocsz % PGSIZE)
+    return -1;
+  
+  oldsz = curproc->sz;
+  newsz = oldsz + allocsz;
+  if ((newsz = vm_ssualloc(curproc->pgdir, oldsz, newsz)) < 0)
+    return -1;
+  curproc->sz = newsz; // 할당받은 크기로 갱신
+  switchuvm(curproc);
+  return oldsz; // 할당받은 메모리 시작 가상주소
+}
+
+int
+sys_getvp(void)
+{
+  pde_t *pgdir = myproc()->pgdir;
+
+  return vm_getvp(pgdir);
+}
+
+int
+sys_getpp(void)
+{
+  pde_t *pgdir = myproc()->pgdir;
+  
+  return vm_getpp(pgdir);
+}
